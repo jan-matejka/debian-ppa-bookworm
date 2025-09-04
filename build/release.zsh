192a3999ac0ec008cd4c2474ce3279914d01a16c
Jan Matějka <jan@matejka.ninja>
2025-09-04 14:45:04 +0200

    feat(build/release.zsh): support multiple distributions

--- build/release.zsh
+++ build/release.zsh
@@ -4,23 +4,25 @@ set -eu
 
 KEY=${1:?}
 
-dist=dists/bookworm
+dists=( bookworm trixie )
 arches=( all amd64 )
 
-pushd -q $dist
-pushd -q main
-dpkg-scanpackages --multiversion . | gzip -c - > Packages.gz
-
-for arch in $arches; do
-  pushd -q binary-$arch
+for dist in $dists; do
+  pushd -q dists/$dist
+  pushd -q main
   dpkg-scanpackages --multiversion . | gzip -c - > Packages.gz
+
+  for arch in $arches; do
+    pushd -q binary-$arch
+    dpkg-scanpackages --multiversion . | gzip -c - > Packages.gz
+    popd -q
+  done
+
+  popd -q
+
+  { cat Release.in; apt-ftparchive release . } > Release
+  gpg --default-key "${KEY}" -abs -o - Release > Release.gpg
+  gpg --default-key "${KEY}" --clearsign -o - Release > InRelease
+
   popd -q
 done
-
-popd -q
-
-{ cat Release.in; apt-ftparchive release . } > Release
-gpg --default-key "${KEY}" -abs -o - Release > Release.gpg
-gpg --default-key "${KEY}" --clearsign -o - Release > InRelease
-
-popd -q
